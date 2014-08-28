73395c9b3914467dfda512061290409fb2b77750
Amoreena Hagedorn & Justin Pacubas <student@epicodus.com>
2014-08-28 15:18:41 -0700

    update lesson class

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,11 +1,36 @@
 class LessonsController < ApplicationController
   def index
     @lessons = Lesson.all
+    @lesson = Lesson.new
     render('lessons/index.html.erb')
   end
 
   def show
     @lesson = Lesson.find(params[:id])
     render('lessons/show.html.erb')
+  end
+
+  def create
+    @lessons = Lesson.all
+    @lesson = Lesson.new(params[:lesson])
+    if @lesson.save
+      redirect_to("/lessons/#{@lesson.id}")
+    else
+      render('lessons/index.html.erb')
+    end
+  end
+
+  def edit
+    @lesson = Lesson.find(params[:id])
+    render('lessons/edit.html.erb')
+  end
+
+  def update
+    @lesson = Lesson.find(params[:id])
+    if @lesson.update(params[:lesson])
+      redirect_to("/lessons/#{@lesson.id}")
+    else
+      render('lessons/edit.html.erb')
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   match('lessons', {:via => :get, :to => 'lessons#index' })
   match('lessons/:id', {via: :get, to: 'lessons#show'})
+  match('lessons/new', {via: :get, to: 'lessons#index'})
+  match('lessons', {via: :post, to: 'lessons#create'})
+  match('lessons/:id/edit', {via: :get, to: 'lessons#edit'})
+  match('lessons/:id', {via: [:patch, :put], to: 'lessons#update'})
 end
